d844db0e238235277d0eecc3666820a50fdef924
Luke Tudge <luketudge@gmail.com>
2019-04-16 09:29:32 +0200

    Adding the first basic examples.

---
+++ basic/age_next_year.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Our first example program.
+
+We can use this space here to provide a short description of our program.
+For example:
+
+A program to get a user's age and tell them their age in one year.
+"""
+
+# We use comments to provide information about the workings of our program.
+# Comments are preceded by #.
+# This tells Python not to try to run the comment line as Python commands.
+
+# The first thing we do in this program is get the user's age.
+# The input() function prints a prompt at the console.
+# Whatever the user stores there becomes the output of the input() function.
+# In this case, we store the output in a variable that we call 'age'.
+age = input('Age please: ')
+
+# Any user input collected with input() comes in the form of a string.
+# ('string' means text).
+# This is the case even if the user types numbers.
+# Python is strict about treating data as numbers or text.
+# We can't just do math with a piece of text.
+# So first we must convert the user's input into an int.
+# ('int' means integer, i.e. a whole number).
+age = int(age)
+
+# Now we can use the print() function to display some printout.
+# Anything we print will be displayed in the console.
+# Here we print first some explanatory text.
+# Text in Python must be enclosed in quotes ''.
+# Otherwise Python would think we are referring to variables or functions.
+# Then we print the answer, using the 'age' variable that we assigned above.
+print('In one year you will be:')
+print(age + 1)
---
+++ basic/name_stuff.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Our second example program.
+    
+A program to get the user's name and tell them some obvious things.
+"""
+
+# Get and store the user's name.
+name = input('Name please: ')
+
+# Print their name ten times.
+# The multiplication operator * works on strings. It repeats them.
+print('Hello')
+print(name * 10)
+
+# Print the first letter of their name.
+# The square parentheses [] pick out single characters from a string.
+# Python begins its numbering at 0, so 0 is the first character.
+print('The first letter of your name is:')
+print(name[0])
+
+# Print out how many letters their name has.
+# The len() function  tells us how many characters a string contains.
+# ('len' is short for 'length'.)
+print('Your name has this many letters:')
+print(len(name))
